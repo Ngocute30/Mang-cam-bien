444bd643868c9c956d516c5d114df3446d9027e9
DESKTOP-C6PDEAT\Admin <hongngocvthn04@gmail.com>
2026-02-05 22:27:41 +0700

    BTH1_MCB_Ngoc

---
+++ BTH1_B22DCDT219_Vu Thi Hong Ngoc/src/i2c.cpp
@@ -0,0 +1,67 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+#define LCD_ADDR 0x27 // đổi 0x3F nếu không hiện
+#define LCD_COLS 16
+#define LCD_ROWS 2
+
+LiquidCrystal_I2C lcd(LCD_ADDR, LCD_COLS, LCD_ROWS);
+
+void setup()
+{
+    Wire.begin(21, 22);
+
+    lcd.init();      // khởi tạo LCD
+    lcd.backlight(); // bật đèn nền
+
+    lcd.setCursor(0, 0);
+    lcd.print("MCB Hong Ngoc");
+
+    lcd.setCursor(0, 1);
+    lcd.print("ESP32 I2C LCD");
+}
+
+void loop()
+{
+    // không cần làm gì thêm
+}
+
+// #include <Arduino.h>
+// #include <Wire.h>
+// #include <BH1750.h>
+
+// BH1750 lightMeter(0x23);
+
+// void setup() {
+//   Serial.begin(9600);
+//   delay(1000);
+
+//   Wire.begin(21, 22);
+//   Wire.setClock(100000);   // I2C standard mode
+
+//   Serial.println("Init BH1750...");
+
+//   if (!lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
+//     Serial.println("BH1750 NOT FOUND");
+//     while (1) {
+//       delay(1000);
+//     }
+//   }
+
+//   Serial.println("BH1750 OK");
+// }
+
+// void loop() {
+//   float lux = lightMeter.readLightLevel();
+
+//   if (lux < 0) {
+//     Serial.println("Read error");
+//   } else {
+//     Serial.print("Cuong do anh sang: ");
+//     Serial.print(lux);
+//     Serial.println(" lux");
+//   }
+
+//   delay(1000);
+// }
---
+++ BTH1_B22DCDT219_Vu Thi Hong Ngoc/src/spi.cpp
@@ -0,0 +1,32 @@
+// #include <SPI.h>
+// #include <MFRC522.h>
+
+// #define SS_PIN 5
+// #define RST_PIN 27
+
+// MFRC522 rfid(SS_PIN, RST_PIN);
+
+// void setup()
+// {
+//     Serial.begin(9600);
+//     SPI.begin(18, 19, 23, 5);
+//     rfid.PCD_Init();
+//     Serial.println("RFID ready");
+// }
+
+// void loop()
+// {
+//     if (!rfid.PICC_IsNewCardPresent())
+//         return;
+//     if (!rfid.PICC_ReadCardSerial())
+//         return;
+//     Serial.print("UID: ");
+//     for (byte i = 0; i < rfid.uid.size; i++)
+//     {
+//         Serial.print(rfid.uid.uidByte[i] < 0x10 ? "0" : "");
+//         Serial.print(rfid.uid.uidByte[i], HEX);
+//         Serial.print(" ");
+//     }
+//     Serial.println();
+//     rfid.PICC_HaltA();
+// }
